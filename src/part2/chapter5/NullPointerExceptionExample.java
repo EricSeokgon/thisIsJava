5f452caaaee2515ce9e4f6bce4c8880e23cd073f
hadesyi <leesk55@gmail.com>
2022-10-17 16:26:04 +0900

    NullPointerException

---
+++ src/part2/chapter5/NullPointerExceptionExample.java
@@ -0,0 +1,12 @@
+package part2.chapter5;
+
+public class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        int[] intArray = null;
+        //intArray[0] = 10; //NullPointerException
+
+        String str = null;
+        // System.out.println("총 문자 수 : "+str.length()); //NullPointerException
+
+    }
+}
