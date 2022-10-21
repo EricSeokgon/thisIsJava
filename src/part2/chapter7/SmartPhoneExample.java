13a025060ca3ece366954c639ea2a466174b066c
hadesyi <leesk55@gmail.com>
2022-10-21 12:57:45 +0900

    기본 생성자 생성 테스트

---
+++ src/part2/chapter7/SmartPhoneExample.java
@@ -0,0 +1,10 @@
+package part2.chapter7;
+
+public class SmartPhoneExample {
+    public static void main(String[] args) {
+        SmartPhone myPhone = new SmartPhone("갤럭시", "은핵");
+
+        System.out.println("모델: "+myPhone.model);
+        System.out.println("색상: "+myPhone.color);
+    }
+}
