87d63006f8c3acdcdd750787e96fe4f1ab744d02
hadesyi <leesk55@gmail.com>
2022-10-21 12:52:41 +0900

    기본 생성자 생성

---
+++ src/part2/chapter7/Phone.java
@@ -0,0 +1,13 @@
+package part2.chapter7;
+
+public class Phone {
+    //필드 선언
+    public String model;
+    public String color;
+
+    //기본 생성자 선언
+
+    public Phone() {
+        System.out.println("Phone() 생성자 실행");
+    }
+}
