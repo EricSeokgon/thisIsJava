1bad2041267e7c87719c43ce18b3a3e4b67014a8
hadesyi <leesk55@gmail.com>
2022-10-24 16:55:54 +0900

    RemoteControlExample

---
+++ src/part2/chapter8/RemoteControlExample.java
@@ -0,0 +1,17 @@
+package part2.chapter8;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        RemoteControl rc;
+
+        rc = new Television();
+        rc.turnOn();
+        rc.setVolume(5);
+        rc.turnOff();
+
+        rc = new Audio();
+        rc.turnOn();
+        rc.setVolume(5);
+        rc.turnOff();
+    }
+}
