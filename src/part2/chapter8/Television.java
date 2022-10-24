04e403f0ef7c56c01ede6207fd862aabad0f22fc
hadesyi <leesk55@gmail.com>
2022-10-24 16:52:36 +0900

    Televison 구현 클래스

--- src/part2/chapter8/Television.java
+++ src/part2/chapter8/Television.java
@@ -1,4 +1,27 @@
 package part2.chapter8;
 
-public class Television {
+public class Television implements RemoteControl {
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("TV를 컵니다.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV를 끕니다.");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        if (volume > RemoteControl.MAX_VOLUME) {
+            this.volume = RemoteControl.MAX_VOLUME;
+        } else if (volume < RemoteControl.MIN_VOLUME) {
+            this.volume = RemoteControl.MIN_VOLUME;
+        } else {
+            this.volume = volume;
+        }
+        System.out.println("현재 TV 볼륨 :" + volume);
+    }
 }
